9d7295a73867c352ed02e0c01cc0ea7fb7062b97
John Napiorkowski <jjn1056@yahoo.com>
2011-01-27 15:03:21 -0500

    less test requirements to reduce the installation debt

--- t/lib/TestApp/View/HTML/Root.pm
+++ t/lib/TestApp/View/HTML/Root.pm
@@ -1,8 +1,8 @@
 package TestApp::View::HTML::Root;
 use Moose;
-use Method::Signatures::Simple;
 
-method main ($stash) {
+sub main {
+    my ($self, $stash) = @_;
     $_->select("#name")->replace_content($stash->{name});
 }
 
